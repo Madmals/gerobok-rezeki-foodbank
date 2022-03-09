f31519416b9713707cf4b6a208bad58ab450498b
@mdmal <madmalzb@gmail.com>
2022-03-09 15:13:01 +0800

    add first home area

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <Add />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Add from "@/components/Add.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Add,
+  },
+};
+</script>
